e3c93a220e5ab239715b0fa00a36cc5b9e9b8e07
Jonathan Felchlin <jonathan@Jonathan-savvy-mbp.local>
2016-07-01 15:28:20 -0700

    Adding data file

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(name='vcs-rosetta',
       author_email='jonathan@xgecko.com',
     #   url='https://www.',
       packages=['translator'],
+      data_files=[('translations', ['translations/git_hg.yaml'])],
       scripts=[
           'scripts/g2h',
           'scripts/h2g'
