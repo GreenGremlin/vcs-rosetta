23612b8bb12fa26cae2de94c83af1a23c0c9223a
Jonathan Felchlin <jonathan@xgecko.com>
2016-07-05 17:38:13 -0700

    Updating some info for PyPi publishing

--- setup.py
+++ setup.py
@@ -4,11 +4,15 @@ setup(name='vcsrosetta',
       description='Version Control System translator - includes g2h (git to hg) and h2g (hg to git)',
       author='Jonathan Felchlin',
       author_email='jonathan@xgecko.com',
-    #   url='https://www.',
+      url = 'https://github.com/GreenGremlin/vcs-rosetta',
+      download_url = 'https://github.com/GreenGremlin/vcs-rosetta/tarball/0.1',
       packages=['vcsrosetta'],
       package_data={'vcsrosetta': ['data/git_hg.yaml']},
       scripts=[
           'scripts/g2h',
           'scripts/h2g'
-      ]
+      ],
+      version = '0.1',
+      keywords = ['git', 'mercurial', 'hg', 'rosetta'], # arbitrary keywords
+      classifiers = []
 )
