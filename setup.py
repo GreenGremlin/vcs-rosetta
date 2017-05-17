f78c5aa47aa50687c39b88b810eb6d356b8f9253
Jonathan Felchlin <jonathan@energsavvy.com>
2017-05-17 01:56:08 -0700

    Updates for publishing

--- setup.py
+++ setup.py
@@ -4,21 +4,21 @@ setup(name='vcsrosetta',
       description='Version Control System translator - includes g2h (git to hg) and h2g (hg to git)',
       author='Jonathan Felchlin',
       author_email='jonathan@xgecko.com',
-      url = 'https://github.com/GreenGremlin/vcs-rosetta',
-      download_url = 'https://github.com/GreenGremlin/vcs-rosetta/tarball/0.1',
+      url='https://github.com/GreenGremlin/vcs-rosetta',
+      download_url='https://github.com/GreenGremlin/vcs-rosetta/tarball/0.1',
       packages=['vcsrosetta'],
       package_data={'vcsrosetta': ['data/git_hg.yaml']},
       scripts=[
           'scripts/g2h',
           'scripts/h2g'
       ],
-      version = '0.1.1',
-      license = 'MIT',
-      keywords = ['git', 'mercurial', 'hg', 'rosetta', 'translate', 'translation'], # arbitrary keywords
-      classifiers = [
+      version='0.1.1',
+      license='MIT',
+      keywords=['git', 'mercurial', 'hg', 'rosetta', 'translate', 'translation'], # arbitrary keywords
+      classifiers=[
           'Development Status :: 3 - Alpha',
           'Intended Audience :: Developers',
-          'Topic :: Software Development :: Development Tools',
+          'Topic :: Software Development :: Documentation',
           'License :: OSI Approved :: MIT License',
           'Programming Language :: Python :: 2',
           'Programming Language :: Python :: 2.6',
